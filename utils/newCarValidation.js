17e5d037a52048efa9446e8f5005ca8a5300aa06
RizqGyx <mhdrizky32@gmail.com>
2024-03-18 20:12:14 +0700

    Adding: Generate ID[POST]

--- utils/newCarValidation.js
+++ utils/newCarValidation.js
@@ -1,3 +1,27 @@
-const carPostValidation = (method) => {};
+const carPostValidation = (method, cars) => {
+  const formatKeys = [
+    "plate",
+    "manufacture",
+    "model",
+    "image",
+    "rentPerDay",
+    "capacity",
+    "description",
+    "availableAt",
+    "transmission",
+    "available",
+    "type",
+    "year",
+    "options",
+    "specs",
+  ];
+
+  if (
+    keys.length !== formatKeys.length ||
+    !exampleKeys.every((key) => keys.includes(key))
+  ) {
+    return res.status(400).json({ error: "Invalid data format" });
+  }
+};
 
 module.exports = { carValidation };
